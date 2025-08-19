8f7e9c4c8773de13b55d407ccd1f6065fae2d339
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2025-08-20 00:36:16 +0100

    Corrige a verificação de grau em beSigned para lançar exceção quando o grau do burocrata for muito baixo.

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 07:38:51 by ansebast          #+#    #+#             */
-/*   Updated: 2025/08/20 00:25:51 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/08/20 00:36:07 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,6 +103,9 @@ void Form::setIsSigned( bool value )
 
 bool Form::beSigned( const Bureaucrat& bureaucrat )
 {
+	if (bureaucrat.getGrade() > 150){
+		throw Form::GradeTooLowException();
+	}
 	if (bureaucrat.getGrade() <= this->signGrade){
 		this->isSigned = true;
 		return (true);
